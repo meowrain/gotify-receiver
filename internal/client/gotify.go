42466e7b46143eaf6f1decfe1bbbed4c823d1e34
shikong <919411476@qq.com>
2024-04-21 00:31:40 +0800

    调整

--- internal/client/gotify.go
+++ internal/client/gotify.go
@@ -3,11 +3,11 @@ package client
 import "gotify-client/pkg/utils/time"
 
 type GotifyMessage struct {
-	Id       int         `json:"id"`
-	Appid    int         `json:"appid"`
-	Message  string      `json:"message"`
-	Title    string      `json:"title"`
-	Priority int         `json:"priority"`
-	Extras   interface{} `json:"extras"`
-	Date     time.Time   `json:"date"`
+	Id       int            `json:"id"`
+	Appid    int            `json:"appid"`
+	Message  string         `json:"message"`
+	Title    string         `json:"title"`
+	Priority int            `json:"priority"`
+	Extras   map[string]any `json:"extras"`
+	Date     time.Time      `json:"date"`
 }
